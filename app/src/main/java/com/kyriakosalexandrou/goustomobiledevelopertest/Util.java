67da871c82298611e3789acc06ab2b36a49d5d7b
KyriakosAlexandrou <kyriakos.a.projects@gmail.com>
2016-02-14 20:38:38 +0000

    Added raw assets of the json responses for the products and categories (this is in case the url for requesting the data becomes invalid you can read the raw data and convert them to objects directly)

--- app/src/main/java/com/kyriakosalexandrou/goustomobiledevelopertest/Util.java
+++ app/src/main/java/com/kyriakosalexandrou/goustomobiledevelopertest/Util.java
@@ -11,6 +11,18 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.kyriakosalexandrou.goustomobiledevelopertest.models.Category;
+import com.kyriakosalexandrou.goustomobiledevelopertest.models.Product;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collection;
+
 /**
  * Created by Kyriakos on 19/01/2016.
  * <p/>
@@ -77,5 +89,49 @@ public class Util {
             throw new AssertionError("LayoutInflater not found.");
         }
         return LayoutInflater;
+    }
+
+
+    public static String loadJSONFromAsset(Context context, String filePath) {
+        String json;
+        try {
+            InputStream is = context.getAssets().open(filePath);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+
+    public static ArrayList<Category> createCategories(Context context) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String categoriesData = loadJSONFromAsset(context, "categories.json");
+
+        Type collectionType = new TypeToken<Collection<Category>>() {
+        }.getType();
+        return gson.fromJson(categoriesData, collectionType);
+    }
+
+    public static ArrayList<Product> createProducts(Context context) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String ProductsData = loadJSONFromAsset(context, "products.json");
+
+        Type collectionType = new TypeToken<Collection<Product>>() {
+        }.getType();
+        return gson.fromJson(ProductsData, collectionType);
+    }
+
+    public static ArrayList<Product> createProductsWithExtras(Context context) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String ProductsWithExtrasData = loadJSONFromAsset(context, "products_with_extras.json");
+
+        Type collectionType = new TypeToken<Collection<Product>>() {
+        }.getType();
+        return gson.fromJson(ProductsWithExtrasData, collectionType);
     }
 }
